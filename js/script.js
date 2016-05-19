27a03eb35047717a60a669b1a2d540ab21f40c41
Matthew Belongia <matthewb@Zipcoder18s-MacBook-Pro.local>
2016-05-19 11:30:36 -0400

    js phonebook

--- js/script.js
+++ js/script.js
@@ -1,28 +1,43 @@
 " use strict ";
+var phoneBook = new Map();
 
 function PhoneBook(){
 
 };
 
+function logName(value,key,map){
+	display.innerHTML += ("name: " + key + "<br />");
+}
+
+function logNumbers(value,key,map){
+	display.innerHTML += ( "number: " + value+ "<br />");
+
+}
 function listAllNames() {
   // code to list all names
+  phoneBook.forEach(logName);
 };
 
 function listAllNumbers() {
   // code to list all numbers
+  phoneBook.forEach(logNumbers);
 };
 
 function showAdd() {
   var name = prompt("Enter full name");
   var number = prompt("Enter phone number");
+  phoneBook.set(name,number);
 };
 
 function showRemove() {
   var remove = prompt("Enter name to remove");
+  phoneBook.delete(remove);
+
 };
 
 function showLookup() {
   var lookup = prompt("Enter number to lookup");
+  display.innerHTML += phoneBook.get(lookup);
 };
 
 var display = document.getElementById("display");
